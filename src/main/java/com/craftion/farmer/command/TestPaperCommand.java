d61a4ad954fc5b2053fe48bfef8d96c8b4c70bc0
Javavailable <vedateray4243@gmail.com>
2026-06-28 09:21:45 +0300

    Test Paper command adapter

---
+++ src/main/java/com/craftion/farmer/command/TestPaperCommand.java
@@ -0,0 +1,10 @@
+package com.craftion.farmer.command;
+
+import io.papermc.paper.command.brigadier.BasicCommand;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+
+public final class TestPaperCommand implements BasicCommand {
+    @Override
+    public void execute(CommandSourceStack source, String[] args) {
+    }
+}
